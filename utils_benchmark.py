fix issues of averaging and normalization

utils_benchmark.py
@@ -139,25 +139,25 @@ def example_data_per_model(humans_norm, gpt_35_100_norm, gpt_4_100_norm, mistral
         features,
         ('Humans', [
             #humans_norm[humans_norm['prompt'] == 'brick'][["originality", "elaboration", "elaboration_SW", "cosine_dist"]].mean().values, # brick
-            humans_norm[humans_norm['prompt'] == 'brick'][[col for col in features]].mean().values, # brick
-            humans_norm[humans_norm['prompt'] == 'box'][[col for col in features]].mean().values, # box
-            humans_norm[humans_norm['prompt'] == 'knife'][[col for col in features]].mean().values, # knife
-            humans_norm[humans_norm['prompt'] == 'rope'][[col for col in features]].mean().values]), # rope
+            humans_norm[humans_norm['prompt'] == 'brick'][features].mean().values, # brick
+            humans_norm[humans_norm['prompt'] == 'box'][features].mean().values, # box
+            humans_norm[humans_norm['prompt'] == 'knife'][features].mean().values, # knife
+            humans_norm[humans_norm['prompt'] == 'rope'][features].mean().values]), # rope
         ('GPT-3', [
-            gpt_35_100_norm[gpt_35_100_norm['prompt'] == 'brick'][[col for col in features]].mean().values,
-            gpt_35_100_norm[gpt_35_100_norm['prompt'] == 'box'][[col for col in features]].mean().values,
-            gpt_35_100_norm[gpt_35_100_norm['prompt'] == 'knife'][[col for col in features]].mean().values,
-            gpt_35_100_norm[gpt_35_100_norm['prompt'] == 'rope'][[col for col in features]].mean().values]),
+            gpt_35_100_norm[gpt_35_100_norm['prompt'] == 'brick'][features].mean().values,
+            gpt_35_100_norm[gpt_35_100_norm['prompt'] == 'box'][features].mean().values,
+            gpt_35_100_norm[gpt_35_100_norm['prompt'] == 'knife'][features].mean().values,
+            gpt_35_100_norm[gpt_35_100_norm['prompt'] == 'rope'][features].mean().values]),
         ('GPT-4', [
-            gpt_4_100_norm[gpt_4_100_norm['prompt'] == 'brick'][[col for col in features]].mean().values,
-            gpt_4_100_norm[gpt_4_100_norm['prompt'] == 'box'][[col for col in features]].mean().values,
-            gpt_4_100_norm[gpt_4_100_norm['prompt'] == 'knife'][[col for col in features]].mean().values,
-            gpt_4_100_norm[gpt_4_100_norm['prompt'] == 'rope'][[col for col in features]].mean().values]),
+            gpt_4_100_norm[gpt_4_100_norm['prompt'] == 'brick'][features].mean().values,
+            gpt_4_100_norm[gpt_4_100_norm['prompt'] == 'box'][features].mean().values,
+            gpt_4_100_norm[gpt_4_100_norm['prompt'] == 'knife'][features].mean().values,
+            gpt_4_100_norm[gpt_4_100_norm['prompt'] == 'rope'][features].mean().values]),
         ('Mistral', [
-            mistral_norm[mistral_norm['prompt'] == 'brick'][[col for col in features]].mean().values,
-            mistral_norm[mistral_norm['prompt'] == 'box'][[col for col in features]].mean().values,
-            mistral_norm[mistral_norm['prompt'] == 'knife'][[col for col in features]].mean().values,
-            mistral_norm[mistral_norm['prompt'] == 'rope'][[col for col in features]].mean().values])
+            mistral_norm[mistral_norm['prompt'] == 'brick'][features].mean().values,
+            mistral_norm[mistral_norm['prompt'] == 'box'][features].mean().values,
+            mistral_norm[mistral_norm['prompt'] == 'knife'][features].mean().values,
+            mistral_norm[mistral_norm['prompt'] == 'rope'][features].mean().values])
     ]
     return data
 
@@ -172,7 +172,7 @@ def radar_charts_per_model(humans_norm, gpt_35_100_norm, gpt_4_100_norm, mistral
     colors = ['b', 'r', 'g', 'm', 'y']
     # Plot the four cases from the example data on separate axes
     for ax, (title, case_data) in zip(axs.flat, data_visu):
-        ax.set_rgrids([0.2, 0.4, 0.6, 0.8])
+        #ax.set_rgrids([0.2, 0.4, 0.6, 0.8])
         #ax.set_rgrids([1, 2, 3, 4, 5])
         ax.set_title(title, weight='bold', size='medium', position=(0.5, 1.1),
                     horizontalalignment='center', verticalalignment='center')
@@ -180,6 +180,9 @@ def radar_charts_per_model(humans_norm, gpt_35_100_norm, gpt_4_100_norm, mistral
             ax.plot(theta, d, color=color)
             ax.fill(theta, d, facecolor=color, alpha=0.25, label='_nolegend_')
         ax.set_varlabels(spoke_labels)
+        
+        ## Line to set the grid
+        ax.set_rticks(np.linspace(0, 0.6, 4))  # Set radial ticks from 0 to 1 with 5 intervals
     # add legend relative to top-left plot
     labels = ('Brick', 'Box', 'Knife', 'Rope')
     legend = axs[0, 0].legend(labels, loc=(0.9, .95),
@@ -191,7 +194,6 @@ def radar_charts_per_model(humans_norm, gpt_35_100_norm, gpt_4_100_norm, mistral
     plt.show()
         
 
-
 ########################################
 ####### 1 radar chart per object #######
 ########################################
@@ -199,32 +201,60 @@ def example_data_per_object(humans_norm, gpt_35_100_norm, gpt_4_100_norm, mistra
     data = [
         features,
         ('Brick', [
-            humans_norm[humans_norm['prompt'] == 'brick'][[col for col in features]].mean().values, # Humans
-            gpt_35_100_norm[gpt_35_100_norm['prompt'] == 'brick'][[col for col in features]].mean().values, # gpt-3.5
-            gpt_4_100_norm[gpt_4_100_norm['prompt'] == 'brick'][[col for col in features]].mean().values, # gpt-4
-            mistral_norm[mistral_norm['prompt'] == 'brick'][[col for col in features]].mean().values]), # mistral
+            humans_norm[humans_norm['prompt'] == 'brick'][features].mean().values, # Humans
+            gpt_35_100_norm[gpt_35_100_norm['prompt'] == 'brick'][features].mean().values, # gpt-3.5
+            gpt_4_100_norm[gpt_4_100_norm['prompt'] == 'brick'][features].mean().values, # gpt-4
+            mistral_norm[mistral_norm['prompt'] == 'brick'][features].mean().values]), # mistral
         ('Box', [
-            humans_norm[humans_norm['prompt'] == 'box'][[col for col in features]].mean().values,
-            gpt_35_100_norm[gpt_35_100_norm['prompt'] == 'box'][[col for col in features]].mean().values,
-            gpt_4_100_norm[gpt_4_100_norm['prompt'] == 'box'][[col for col in features]].mean().values,
-            mistral_norm[mistral_norm['prompt'] == 'box'][[col for col in features]].mean().values]),
+            humans_norm[humans_norm['prompt'] == 'box'][features].mean().values,
+            gpt_35_100_norm[gpt_35_100_norm['prompt'] == 'box'][features].mean().values,
+            gpt_4_100_norm[gpt_4_100_norm['prompt'] == 'box'][features].mean().values,
+            mistral_norm[mistral_norm['prompt'] == 'box'][features].mean().values]),
         ('Knife', [
-            humans_norm[humans_norm['prompt'] == 'knife'][[col for col in features]].mean().values,
-            gpt_35_100_norm[gpt_35_100_norm['prompt'] == 'knife'][[col for col in features]].mean().values,
-            gpt_4_100_norm[gpt_4_100_norm['prompt'] == 'knife'][[col for col in features]].mean().values,
-            mistral_norm[mistral_norm['prompt'] == 'knife'][[col for col in features]].mean().values]),
+            humans_norm[humans_norm['prompt'] == 'knife'][features].mean().values,
+            gpt_35_100_norm[gpt_35_100_norm['prompt'] == 'knife'][features].mean().values,
+            gpt_4_100_norm[gpt_4_100_norm['prompt'] == 'knife'][features].mean().values,
+            mistral_norm[mistral_norm['prompt'] == 'knife'][features].mean().values]),
         ('Rope', [
-            humans_norm[humans_norm['prompt'] == 'rope'][[col for col in features]].mean().values,
-            gpt_35_100_norm[gpt_35_100_norm['prompt'] == 'rope'][[col for col in features]].mean().values,
-            gpt_4_100_norm[gpt_4_100_norm['prompt'] == 'rope'][[col for col in features]].mean().values,
-            mistral_norm[mistral_norm['prompt'] == 'rope'][[col for col in features]].mean().values])
+            humans_norm[humans_norm['prompt'] == 'rope'][features].mean().values,
+            gpt_35_100_norm[gpt_35_100_norm['prompt'] == 'rope'][features].mean().values,
+            gpt_4_100_norm[gpt_4_100_norm['prompt'] == 'rope'][features].mean().values,
+            mistral_norm[mistral_norm['prompt'] == 'rope'][features].mean().values])
     ]
     return data
 
-def radar_charts_per_object(humans_norm, gpt_35_100_norm, gpt_4_100_norm, mistral_norm, features):
+def example_data_per_object_2(brick_norm, box_norm, knife_norm, rope_norm, features):
+    data = [
+        features,
+        ('Brick', [
+            brick_norm[brick_norm['dataset'] == 'Humans'][features].mean().values, # Humans
+            brick_norm[brick_norm['dataset'] == 'GPT-3.5'][features].mean().values, # gpt-3.5
+            brick_norm[brick_norm['dataset'] == 'GPT-4'][features].mean().values, # gpt-4
+            brick_norm[brick_norm['dataset'] == 'Mistral'][features].mean().values]), # mistral
+        ('Box', [
+            box_norm[box_norm['dataset'] == 'Humans'][features].mean().values,
+            box_norm[box_norm['dataset'] == 'GPT-3.5'][features].mean().values,
+            box_norm[box_norm['dataset'] == 'GPT-4'][features].mean().values,
+            box_norm[box_norm['dataset'] == 'Mistral'][features].mean().values]),
+        ('Knife', [
+            knife_norm[knife_norm['dataset'] == 'Humans'][features].mean().values,
+            knife_norm[knife_norm['dataset'] == 'GPT-3.5'][features].mean().values,
+            knife_norm[knife_norm['dataset'] == 'GPT-4'][features].mean().values,
+            knife_norm[knife_norm['dataset'] == 'Mistral'][features].mean().values]),
+        ('Rope', [
+            rope_norm[rope_norm['dataset'] == 'Humans'][features].mean().values,
+            rope_norm[rope_norm['dataset'] == 'GPT-3.5'][features].mean().values,
+            rope_norm[rope_norm['dataset'] == 'GPT-4'][features].mean().values,
+            rope_norm[rope_norm['dataset'] == 'Mistral'][features].mean().values])
+    ]
+    return data
+
+def radar_charts_per_object(brick_norm, box_norm, knife_norm, rope_norm, features):
+#def radar_charts_per_object(humans_norm, gpt_35_100_norm, gpt_4_100_norm, mistral_norm, features):
     N = len(features)
     theta = radar_factory(N, frame='polygon')
-    data_visu = example_data_per_object(humans_norm, gpt_35_100_norm, gpt_4_100_norm, mistral_norm, features)
+    #data_visu = example_data_per_object(humans_norm, gpt_35_100_norm, gpt_4_100_norm, mistral_norm, features)
+    data_visu = example_data_per_object_2(brick_norm, box_norm, knife_norm, rope_norm, features)
     spoke_labels = data_visu.pop(0)
     fig, axs = plt.subplots(figsize=(9, 9), nrows=2, ncols=2,
                             subplot_kw=dict(projection='radar'))
@@ -232,14 +262,19 @@ def radar_charts_per_object(humans_norm, gpt_35_100_norm, gpt_4_100_norm, mistra
     colors = ['b', 'r', 'g', 'm', 'y']
     # Plot the four cases from the example data on separate axes
     for ax, (title, case_data) in zip(axs.flat, data_visu):
-        ax.set_rgrids([0.2, 0.4, 0.6, 0.8])
+        #ax.set_rgrids(np.arange(0, 1.2, 0.2).round(2), labels=np.arange(0, 1.2, 0.2).round(2))
+        ax.set_varlabels(spoke_labels)
         #ax.set_rgrids([1, 2, 3, 4, 5])
         ax.set_title(title, weight='bold', size='medium', position=(0.5, 1.1),
                     horizontalalignment='center', verticalalignment='center')
         for d, color in zip(case_data, colors):
             ax.plot(theta, d, color=color)
             ax.fill(theta, d, facecolor=color, alpha=0.25, label='_nolegend_')
-        ax.set_varlabels(spoke_labels)
+        #ax.set_varlabels(spoke_labels)
+        
+        ## Line to set the grid
+        ax.set_rticks(np.linspace(0, 0.6, 4))  # Set radial ticks from 0 to 1 with 5 intervals
+    
     # add legend relative to top-left plot
     labels = ('Humans', 'GPT-3.5', 'GPT-4', 'Mistral')
     legend = axs[0, 0].legend(labels, loc=(0.9, .95),
@@ -254,9 +289,9 @@ def radar_charts_per_object(humans_norm, gpt_35_100_norm, gpt_4_100_norm, mistra
 ########### 1 chart in total ###########
 ########################################
 
-def plot_radar_chart(dataframes, titles):
+def plot_radar_chart(dataframes, titles, avg_per_object):
     # Define categories (assuming all dataframes have the same columns)
-    categories = list(dataframes[0].columns)
+    categories = list(dataframes[0].columns)[1:] # to not take into account the prompt column
     num_categories = len(categories)
 
     # Create angles for radar chart
@@ -268,14 +303,27 @@ def plot_radar_chart(dataframes, titles):
 
     # Plot each dataframe as a polygon on the radar chart
     for i, df in enumerate(dataframes):
-        values = df.mean().values.flatten().tolist()
+        
+        if avg_per_object == True:
+            # dimensions averaged by considering different objects
+            values = []
+            for object in ['brick', 'box', 'knife', 'rope']:
+                df_object = df[df['prompt'] == object]
+                # average per object
+                values += df_object[categories].mean().values.flatten().tolist()
+            # average across the 4 objects
+            values = np.array(values).reshape(4, 4).mean(axis=0).tolist()
+        else:
+            # dimensions averaged without considering different objects
+            values = df[categories].mean().values.flatten().tolist()
+        
         values += values[:1]  # To close the circle for radar chart
         #ax.fill(angles, values, alpha=0.2, label=titles[i]) # Plot by filling polygons
         ax.plot(angles, values, label=titles[i])  # Plot edges only
 
     # Set radial ticks and labels
-    ax.set_yticks(np.arange(0, 1.2, 0.2).round(2))  # Set radial ticks
-    ax.set_yticklabels(np.arange(0, 1.2, 0.2).round(2), fontsize='small', color='gray')  # Set radial labels
+    ax.set_yticks(np.arange(0, 0.7, 0.2).round(2))  # Set radial ticks
+    ax.set_yticklabels(np.arange(0, 0.7, 0.2).round(2), fontsize='small', color='gray')  # Set radial labels
 
     ax.set_xticks(angles[:-1])
     ax.set_xticklabels(categories)
@@ -286,3 +334,50 @@ def plot_radar_chart(dataframes, titles):
     plt.title('LLMs vs Humans on the AUT')
     plt.show()
         
+
+########################################
+########### Normalization ##############
+########################################
+
+def normalization_1(df, check_norm):
+    # normalize by features: min max scaling
+    result = df.copy()
+    for feature_name in df.columns:
+        if feature_name in ["originality", "elaboration", "elaboration_SW", "cosine_dist"]:
+            max_value = df[feature_name].max()
+            min_value = df[feature_name].min()
+            result[feature_name] = (df[feature_name] - min_value) / (max_value - min_value)
+    
+    if check_norm:
+        features = ['originality', 'elaboration', 'elaboration_SW', 'cosine_dist']
+
+        # before normalization
+        fig, axs = plt.subplots(1, 4, figsize = (15,4))
+        for i, dim in enumerate(features):
+            sns.boxplot(data=df, y=dim, x='dataset', ax=axs[i])
+
+        plt.suptitle("Before normalization")
+        plt.tight_layout()
+        plt.show()
+
+        # after normalization
+        fig, axs = plt.subplots(1, 4, figsize = (15,4))
+        for i, dim in enumerate(features):
+            sns.boxplot(data=result, y=dim, x='dataset', ax=axs[i])
+
+        plt.suptitle("After normalization")
+        plt.tight_layout()
+        plt.show()
+    
+    return result
+
+def normalization_per_model(df):
+    # normalize by features: min max scaling
+    result = df.copy()
+    for feature_name in df.columns:
+        if feature_name in ["originality", "elaboration", "elaboration_SW", "cosine_dist"]:
+            max_value = df[feature_name].max()
+            min_value = df[feature_name].min()
+            result[feature_name] = (df[feature_name] - min_value) / (max_value - min_value)
+    
+    return result